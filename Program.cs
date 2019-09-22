d3f2f017af3b63dcb02b33f27d958b7b67657288
asmith0 <alexmsmith0@gmail.com>
2019-09-22 20:23:49 +0100

    Program.cs - Replace .csv CRUD functionality with SQL Querying.   - Establish a connection to the database via variable 'connectionString'.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+using System.Data.SqlClient;
+using System.Data;
+
 namespace StudentRecordSystem
 {
     /// <summary>
@@ -13,7 +16,8 @@ namespace StudentRecordSystem
     /// </summary>
     class Program
     {
-        public static string path = @"C:\Users\alexm\Documents\records.csv";
+        /** Connection To SQL Server **/
+        public static string connectionString = "Server=localhost;Database=student_system;Trusted_Connection=True;";
         public static int counter = 0;
         public static DateTime localDate = DateTime.Now;
         struct Student
@@ -81,7 +85,7 @@ namespace StudentRecordSystem
             } while (exit == false);
         }
 
-        // Insert a new student record.
+        /** Insert A New Student Record **/
         public static void Insert()
         {
             Console.Write("Enter Student First Name: ");
@@ -93,25 +97,29 @@ namespace StudentRecordSystem
 
             Console.WriteLine("Inserted.");
 
-            // Increment counter when a record has been inserted.
-            ++counter;
+            SqlConnection conn = new SqlConnection(connectionString);
 
+            // When inserting a new record into the table, the id doesn't auto-increment as unique value.
+            // For the time being, calculate the amount of rows within the student_records table and set this counter to the new record id.
             try
             {
-                using (StreamWriter sw = File.AppendText(path))
-                {
-                    sw.WriteLine(counter + "," + fname + "," + lname + "," + grade + "," + localDate + "," + localDate + "," + "(Null)?");
-                }
+                SqlCommand cmd = new SqlCommand("INSERT INTO student_records (id, first_name, last_name, grade, created_at, updated_at) VALUES (1, @fname, @lname, @grade, @localDate, @localDate)", conn);
+                cmd.Parameters.AddWithValue("@fname", fname);
+                cmd.Parameters.AddWithValue("@lname", lname);
+                cmd.Parameters.AddWithValue("@grade", grade);
+                cmd.Parameters.AddWithValue("@localDate", localDate);
+                SqlDataAdapter sqlda = new SqlDataAdapter(cmd);
+                DataTable dbtl = new DataTable();
+                sqlda.Fill(dbtl);
             }
-            catch (IOException e)
+            catch (Exception e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine("Could Not Open Connection.");
                 Console.WriteLine(e.Message);
             }
         }
 
-        // Edit an existing student record.
-        // Takes in a student id argument.
+        /** Edit An Existing Student Record **/
         public static void Edit()
         {
             Console.WriteLine("Enter the record id you would like to edit: ");
@@ -119,245 +127,164 @@ namespace StudentRecordSystem
 
             Console.WriteLine();
             Console.WriteLine("Please specific which column you would like to edit: ");
-            Console.WriteLine("Firstname - 1 : Lastname - 2 : Grade - 3:");
-            var columnNumber = Console.ReadLine();
+            var column = Console.ReadLine();
 
             Console.WriteLine();
             Console.Write("Replace with: ");
-            var columnNewValue = Console.ReadLine();
+            string value = Console.ReadLine();
 
-            // Line that we need to edit (found through searchId input).
-            string record = "";
-            StringBuilder sb = new StringBuilder();
+            SqlConnection conn = new SqlConnection(connectionString);
 
-            // Read the individual lines of the .csv file.
             try
             {
-                StreamReader sr = new StreamReader(path);
+                SqlCommand cmd = new SqlCommand("UPDATE student_records SET [" + column + "]=@value WHERE id=@searchId", conn);
+                cmd.Parameters.AddWithValue("@value", value);
+                cmd.Parameters.AddWithValue("@searchId", searchId);
+                SqlDataAdapter sqlda = new SqlDataAdapter(cmd);
+                DataTable dbtl = new DataTable();
+                sqlda.Fill(dbtl);
 
-                string line;
-                // Loop through each of these lines
-                while ((line = sr.ReadLine()) != null) {
-                    // If id column matches search id, delete that record
-                    if (line.Split(',')[0] == searchId)
-                    {
-                        // Assign this record to b.
-                        record = line;
-                    }
-                    // Otherwise, append the remaining records to the string builder.
-                    else
-                    {
-                        sb.Append(line);
-                    }
-                }
-
-                sr.Close();
-
-                // When the record has been updated, we want to update the timestamp of the 'updated' column for that record.
-
-                // Selected column original value to edit in record.
-                string columnBefore = record.Split(',')[int.Parse(columnNumber)];
-
-                StreamWriter sw = new StreamWriter(path);
-                // Loop through all columns until column to edit, skip this and process to appending the stringbuilder with other columns in loop.
-                for (var i = 0; i <= 6; i++)
-                {
-                    if (i == int.Parse(columnNumber))
-                    {
-                        sb.Append(columnNewValue + ",");
-                    }
-                    // Give 'updated' edit record timestamp.
-                    else if (i == 5)
-                    {
-                        sb.Append(localDate + ",");
-                    } 
-                    // Remove trailing comma of string builder.
-                    else if (i == 6)
-                    {
-                        sb.Append(record.Split(',')[i]);
-                    }
-                    else
-                    {
-                        sb.Append(record.Split(',')[i] + ",");
-                    }
-                }
-
-                // We want to avoid spliting the last record, because this will cause a blank record to begin.
-                string[] o = sb.ToString().Split('?');
-
-                for (var i = 0; i < o.Count() - 1; i++)
-                {
-                    sw.WriteLine(o[i] + '?');
-                }
-                sw.Close();
-
+                conn.Close();
             }
-            catch (IOException e)
+            catch (Exception e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine("Could Not Open Connection.");
                 Console.WriteLine(e.Message);
             }
-
         }
 
-        // Delete an existing student record.
-        // Takes in a student id argument.
+        /** Delete An Existing Student Record **/
         public static void Delete()
         {
             Console.Write("Enter the record id you would like to delete: ");
             var searchId = Console.ReadLine();
 
-            // This will store the line which needs to be soft-deleted.
-            string record = "";
-            StringBuilder sb = new StringBuilder();
+            SqlConnection conn = new SqlConnection(connectionString);
 
-            // Read the individual lines of the .csv file.
-            using (StreamReader sr = new StreamReader(path))
+            try
             {
-                string line;
-                // Loop through each of these lines.
-                while ((line = sr.ReadLine()) != null)
-                {
-                    // If id column matches search id, delete that record.
-                    if (line.Split(',')[0] == searchId)
-                    {
-                        record = line;
-                    }
-                    else
-                        sb.Append(line);
-                        continue;
-                }
+                SqlCommand cmd = new SqlCommand("UPDATE student_records SET deleted_at=@localDate WHERE id=@searchId", conn);
+                cmd.Parameters.AddWithValue("@localDate", localDate);
+                cmd.Parameters.AddWithValue("@searchId", searchId);
+                SqlDataAdapter sqlda = new SqlDataAdapter(cmd);
+                DataTable dtbl = new DataTable();
+                sqlda.Fill(dtbl);
+
+                conn.Close();
             }
-            // Check if record has already been soft-deleted
-            if (record.Split(',')[6] != "(Null)?")
-                Console.WriteLine("This record has already been soft-deleted.");
-            else
+            catch (Exception e)
             {
-                try
-                {
-                    using (StreamWriter sw = new StreamWriter(path))
-                    {
-                        for (var i = 0; i <= 5; i++)
-                        {
-                            sb.Append(record.Split(',')[i] + ",");
-                        }
-                        sb.Append(localDate);
-
-                        string[] o = sb.ToString().Split('?');
-                        foreach (var w in o)
-                        {
-                            sw.WriteLine(w + '?');
-                        }
-                    }
-                }
-                catch (IOException e)
-                {
-                    Console.WriteLine("The file could not be read:");
-                    Console.WriteLine(e.Message);
-                }
+                Console.WriteLine("Could Not Open Connection.");
+                Console.WriteLine(e.Message);
             }
         }
 
-        // Read an existing student record.
-        // Takes in a student id argument.
+        /** Read A Student Record From Database With A Specified ID **/
         public static void Read()
         {
             Console.Write("Enter the record id you would like to read: ");
             var searchId = Console.ReadLine();
 
-            // Run an exception, to check if the .csv file can be read.
+            SqlConnection conn = new SqlConnection(connectionString);
+
             try
             {
-                var strLines = File.ReadLines(path);
-                foreach (var line in strLines)
+                SqlCommand cmd = new SqlCommand("SELECT * FROM student_records WHERE id=@searchId", conn);
+                cmd.Parameters.AddWithValue("@searchId", searchId);
+
+                SqlDataAdapter sqlda = new SqlDataAdapter(cmd);
+
+                DataTable dtbl = new DataTable();
+                sqlda.Fill(dtbl);
+
+                foreach (DataRow row in dtbl.Rows)
                 {
-                    if (line.Split(',')[0].Equals(searchId))
-                        Console.WriteLine(line);
+                    Console.WriteLine(String.Format("|{0,3}|{1,6}|{2,6}|{3,5}|{4,6}|{5,6}|{6,6}", row["id"], row["first_name"], row["last_name"], row["grade"], row["created_at"], row["updated_at"], row["deleted_at"]));
                 }
+
+                conn.Close();
             }
-            catch (IOException e)
+            catch (Exception e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine("Could Not Open Connection.");
                 Console.WriteLine(e.Message);
             }
         }
 
-        // Only read records which haven't been soft-deleted.
+        /** Only Read Records Which Haven't Been Soft-Deleted **/
         public static void ReadNonDeleted()
         {
+            SqlConnection conn = new SqlConnection(connectionString);
+
             try
             {
-                StreamReader all = new StreamReader(File.OpenRead(path));
-
-                string line;
-                // Loop through each of these lines.
-                while ((line = all.ReadLine()) != null)
+                SqlCommand cmd = new SqlCommand("SELECT * FROM student_records WHERE deleted_at IS NULL", conn);
+                SqlDataAdapter sqlda = new SqlDataAdapter(cmd);
+                DataTable dbtl = new DataTable();
+                sqlda.Fill(dbtl);
+                foreach (DataRow row in dbtl.Rows)
                 {
-                    if (line.Split(',')[6] == "(Null)?")
-                        Console.WriteLine("LINE: " + line);
-                    else
-                        continue;
+                    Console.WriteLine(String.Format("|{0,3}|{1,6}|{2,6}|{3,5}|{4,6}|{5,6}|{6,6}", row["id"], row["first_name"], row["last_name"], row["grade"], row["created_at"], row["updated_at"], row["deleted_at"]));
                 }
-                all.Close();
+
+                conn.Close();
             }
-            catch (IOException e)
+            catch (Exception e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine("Could Not Open Connection.");
                 Console.WriteLine(e.Message);
             }
         }
 
-        // Only read records which have been soft-deleted.
+        /** Only Read Records Which Have Been Soft-Deleted **/
         public static void ReadSoftDeleted()
         {
+            SqlConnection conn = new SqlConnection(connectionString);
+
             try
             {
-                StreamReader all = new StreamReader(File.OpenRead(path));
-
-                string line;
-                // Skips over the first line, since it contains columns names.
-                all.ReadLine();
-                // Loop through each of these lines.
-                while ((line = all.ReadLine()) != null)
+                SqlCommand cmd = new SqlCommand("SELECT * FROM student_records WHERE deleted_at IS NOT NULL", conn);
+                SqlDataAdapter sqlda = new SqlDataAdapter(cmd);
+                DataTable dbtl = new DataTable();
+                sqlda.Fill(dbtl);
+                foreach (DataRow row in dbtl.Rows)
                 {
-                    if (line.Split(',')[6] != "(Null)?")
-                        Console.WriteLine("LINE: " + line);
-                    else
-                        continue;
+                    Console.WriteLine(String.Format("|{0,3}|{1,6}|{2,6}|{3,5}|{4,6}|{5,6}|{6,6}", row["id"], row["first_name"], row["last_name"], row["grade"], row["created_at"], row["updated_at"], row["deleted_at"]));
                 }
-                all.Close();
+
+                conn.Close();
             }
-            catch (IOException e)
+            catch (Exception e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine("Could Not Open Connection.");
                 Console.WriteLine(e.Message);
             }
         }
 
-        // Read all student records.
+        /** Read All Student Records From Database **/
         public static void All()
         {
+            Console.WriteLine(string.Format("|{0,3}|{1,6}|{2,6}|{3,5}|{4,6}|{5,6}|{6,6}", "Id", "First Name", "Last Name", "Grade", "Created At", "Updated At", "Deleted At"));
+            Console.WriteLine("------------------------------");
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
             try
             {
-                StreamReader all = new StreamReader(File.OpenRead(path));
-
-                while (!all.EndOfStream)
+                SqlCommand cmd = new SqlCommand("SELECT * FROM student_records", conn);
+                SqlDataAdapter sqlda = new SqlDataAdapter(cmd);
+                DataTable dtbl = new DataTable();
+                sqlda.Fill(dtbl);
+                foreach (DataRow row in dtbl.Rows)
                 {
-                    string line = all.ReadLine();
-                    //Console.WriteLine("LINE: " + line);
-                    string[] l = line.Split('?');
-
-                    string la = l[0];
-                    string[] column = la.Split(',');
-                    Console.WriteLine(String.Format("|{0,3}|{1,6}|{2,6}|{3,5}|{4,6}|{5,6}|{6,6}", column[0], column[1].PadRight(25), column[2].PadRight(25), column[3], column[4].PadRight(20), column[5].PadRight(20), column[6].PadRight(20)));
-                    Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine(String.Format("|{0,3}|{1,6}|{2,6}|{3,5}|{4,6}|{5,6}|{6,6}", row["id"], row["first_name"], row["last_name"], row["grade"], row["created_at"], row["updated_at"], row["deleted_at"]));
                 }
 
-                all.Close();
+                conn.Close();
             }
-            catch (IOException e)
+            catch (Exception e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine("Could Not Open Connection.");
                 Console.WriteLine(e.Message);
             }
         }
